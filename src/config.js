3135af9901fb7ee6e80a0c66488c36a599e90939
PandesalPanpan <petermarticio@gmail.com>
2026-02-17 14:33:51 +0800

    fix: convert when salt rounds is not missing

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ configDotenv();
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
 export const SECRET = process.env.SECRET ?? 'maculitiza';
-export const SALT_ROUNDS = process.env.SALT_ROUNDS ?? Number(10);
+export const SALT_ROUNDS = process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10;
 export const DATABASE_URL = process.env.DATABASE_URL;
 
 if (!DATABASE_URL) {
