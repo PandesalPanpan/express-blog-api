c287b91c891a37d0f2fb69188738769c73c38edd
PandesalPanpan <petermarticio@gmail.com>
2026-02-17 14:34:10 +0800

    feat: add userRegisterValidation and validate middleware

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -1,15 +1,23 @@
 import { body, validationResult } from "express-validator";
 
 export const userRegisterValidation = [
-    body("email").isEmail().withMessage("Enter a valid email address."), ,
-    body("username").isLength({ min: 5, max: 16 }).withMessage("Username must be atleast 5 upto 16 characters."),
-    body("password").isLength({ min: 8, max: 32 }).withMessage("Password must be atleast 8 upto 32 characters.")
+    body("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Enter a valid email address."),
+    body("username")
+    .trim()
+    .isLength({ min: 5, max: 16 }).withMessage("Username must be atleast 5 upto 16 characters."),
+    body("password")
+    .isLength({ min: 8, max: 32 }).withMessage("Password must be atleast 8 upto 32 characters.")
 ]
 
+// This probably becomes like a validator middleware or an index validator file
 export const validate = (req, res, next) => {
     const errors = validationResult(req);
-    if (errors.isEmpty) {
-        next();
+    if (errors.isEmpty()) {
+        return next();
     }
     return res.status(400).json({ errors: errors.array() })
 }
